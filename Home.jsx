bd1ef294ed586ae51ecae82ea5d229a3d41f2110
ArisBatumbakal <121299450+ArisBatumbakal@users.noreply.github.com>
2024-04-04 05:48:33 +0700

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,7 +1,8 @@
-import {Container, Box, Divider, Typography,Button} from '@mui/material';
+import {Link} from 'react-router-dom';
+import {Container, Box, Divider, Typography,Button, Grid} from '@mui/material';
 import {Groups, Login, PersonAddAlt1Outlined} from '@mui/icons-material'
 
-function Home(){
+const HomePage = () => {
     return (
         <>
             <Container 
@@ -30,31 +31,60 @@ function Home(){
                 }}>
                     Join the growing community inside this ChatApp
                 </Box>
-                <Box sx={{
-                    display:'grid',
-                    alignItems:'center',
-                    justifyContent:'center',
-                    
-                }}>
-                    <Box>
-                        <Typography variant='body2'>Already a User?</Typography>
-                        <Button>
-                            <Login/>Login
-                        </Button>
-                    </Box>
-                    <Box>
-                        <Typography variant='body2'>or</Typography>
-                    </Box>
-                    <Box>
-                        <Typography variant='body2'>Haven't joined yet?</Typography>
-                        <Button>
-                            <Login/>Login
-                        </Button>
-                    </Box>
-                </Box>
+                <Grid container spacing={2}>
+                    <Grid item xs={3}></Grid>
+                    <Grid item xs={2.5}>
+                        <Typography variant='body2' sx={{fontSize:"1.5rem", fontWeight:"200", pb:'1rem'}}>Already a User?</Typography>
+                        <Link to="/login"><Button variant='contained' color="success" sx={{px:'2rem',py:'0.5rem'}} startIcon={<Login/>}>
+                            Login
+                        </Button></Link>
+                    </Grid>
+                    <Grid item xs={1}>
+                        <Typography variant='body2' sx={{fontSize:"1.5rem", fontWeight:"200"}}>or</Typography>
+                    </Grid>
+                    <Grid item xs={2.5}>
+                        <Typography variant='body2' sx={{fontSize:"1.5rem", fontWeight:"200", pb:'1rem'}}>Haven't joined yet?</Typography>
+                        <Link to="/register"><Button variant='contained' color="success" sx={{px:'2rem',py:'0.5rem'}} startIcon={<PersonAddAlt1Outlined/>}>
+                            Register
+                        </Button></Link>
+                    </Grid>
+                    <Grid item xs={3}></Grid>
+                </Grid>
+                <Divider variant='middle' sx={{my:'50px'}}/>
+                <Grid container gap={3}>
+                    <Grid item xs={1}></Grid>
+                    <Grid item xs={3} sx={{
+                        borderBottom:2,
+                        borderColor:"#154017",
+                        px:'1rem', py:'1rem'
+                    }}>
+                        <Typography className="inconsolata-font" variant='h5' sx={{pb:'20px'}}>ALL USERS</Typography>
+                        <Divider variant='middle'/>
+                        <Typography variant='h3' sx={{py:'20px'}}>12</Typography>
+                    </Grid>
+                    <Grid item xs={3} sx={{
+                        borderBottom:2,
+                        borderColor:"#154017",
+                        px:'1rem', py:'1rem'
+                    }}>
+                        <Typography className="inconsolata-font" variant='h5' sx={{pb:'20px'}}>ACTIVE USERS</Typography>
+                        <Divider variant='middle'/>
+                        <Typography variant='h3' sx={{py:'20px'}}>12</Typography>
+                    </Grid>
+                    <Grid item xs={3} sx={{
+                        borderBottom:2,
+                        borderColor:"#154017",
+                        px:'1rem', py:'1rem'
+                    }}>
+                        <Typography className="inconsolata-font" variant='h5' sx={{pb:'20px'}}>ALL PUBLIC ROOMS</Typography>
+                        <Divider variant='middle'/>
+                        <Typography variant='h3' sx={{py:'20px'}}>12</Typography>
+                    </Grid>
+                    <Grid item xs={1}></Grid>
+                </Grid>
             </Container>
         </>
     )
 }
 
-export default Home
+export default HomePage;
