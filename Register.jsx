9dccf8d1f2b24e161f6f315d24b383f2fc98f6a5
ArisBatumbakal <121299450+ArisBatumbakal@users.noreply.github.com>
2024-04-08 19:38:21 +0200

    Update Register.jsx 8/4

--- Register.jsx
+++ Register.jsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import {Link} from 'react-router-dom';
 import {Container, Avatar, Badge, Box, Divider, FormControl, InputLabel, OutlinedInput, IconButton, InputAdornment, Typography,Button, Grid, TextField} from '@mui/material';
-import {Groups, CheckCircleOutline, EmailOutlined, Login, CameraAltRounded, PersonAddAlt1Outlined, Diversity3, Person, Visibility, VisibilityOff } from '@mui/icons-material';
+import {HowToRegOutlined, EmailOutlined, CameraAltRounded, Visibility, VisibilityOff } from '@mui/icons-material';
+
 
 
 function Register(){
@@ -22,20 +23,42 @@ function Register(){
     const info1Style = {
         textAlign:'left'
     }
+    const [imgSRC, setImgSRC] = React.useState();
+    const [countUChar, setCountUChar] = React.useState('');
+    const refInputTypeFile = React.useRef(null);
+    const handleImgChange = (e) => {
+        setImgSRC(e.target.files[0]);
+    }
+    const [errorStateU, setErrorStateU] = React.useState(false);
+    const [usernameTxt, setUsernameTxt] = React.useState('');
+    const handleUsername = (e) => {
+        let count = e.target.value.length;
+        let uval = e.target.value;
+        if(count <= 31){
+            setUsernameTxt('');
+            setCountUChar(uval);
+            setErrorStateU(false);
+            if(count == 31){
+                setErrorStateU(true);
+                setUsernameTxt("Maximum of 30 characters only!");
+            }
+            
+        }
+    }
 
     return (
         <>
             <Container
                 className='inconsolata-font'
-                sx={{
+                sx={(theme) => ({
                     margin:'auto',
                     width: '98vW',
                     height: '100vH',
                     backgroundColor: '#BDCDBB',
                     p:'2vH', pt:'10vh',
                     color:'#154017',
-                    textAlign:'center',
-                }}>
+                    textAlign:'center'
+                })}>
                 <Box
                     sx={(theme) => ({
                         backgroundColor:'white',
@@ -43,6 +66,7 @@ function Register(){
                         border:'1px grey',
                         boxShadow:'0px 2px 3px grey',
                         borderRadius:'3px', width:'90%',
+                        textAlign:'left',
                         [theme.breakpoints.up('xs')] : {
                             width:'90%', pt:'5%'
                         },[theme.breakpoints.up('md')] : {
@@ -50,32 +74,30 @@ function Register(){
                         }
                     })}>
                         <Typography
-                            sx={(theme) => ({
-                                fontSize:'26px',
-                                pb:'5vh', width:'100%',
-                                [theme.breakpoints.up('xs')] : {
-                                    fontSize:'18px',
-                                },
-                                [theme.breakpoints.up('md')] : {
-                                    fontSize:'26px',
-                                }
-                            })}
+                            sx={{
+                                fontSize:'24px',
+                                textAlign:'center',
+                                pb:'5vh', width:'100%'
+                            }}
                         >Join us and enjoy the environment within this ChatApp</Typography>
 
                         <TextField
                             fullWidth
+                            size='small'
                             label="Register an Email"
                             id="outlined-start-adornment"
                             InputProps={{
                                 startAdornment: <InputAdornment position="start"><EmailOutlined/></InputAdornment>,
+                                style: {color:'#154017', fontWeight:500}
                             }}
                         /><br/>
                         <FormControl sx={{my:2, width: '47%', mr:'3%'}} variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                        <InputLabel sx={{fontSize:'14px', mt:'2px'}} size='small' htmlFor="outlined-adornment-password">Password</InputLabel>
                         <OutlinedInput
+                            size='small'
                             id="outlined-adornment-password"
                             type={showPassword ? 'text' : 'password'}
-                            sx={{fontSize:'14px'}}
+                           
                             endAdornment={
                             <InputAdornment position="end">
                                 <IconButton
@@ -93,11 +115,11 @@ function Register(){
                         </FormControl>
                         
                         <FormControl sx={{my:2, width: '47%', ml:'3%'}} variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
+                        <InputLabel sx={{fontSize:'14px', mt:'2px'}} size='small' htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
                         <OutlinedInput
+                            size='small'
                             id="outlined-adornment-password"
                             type={showCPassword ? 'text' : 'password'}
-                            sx={{fontSize:'14px'}}
                             endAdornment={
                             <InputAdornment position="end">
                                 <IconButton
@@ -114,39 +136,105 @@ function Register(){
                         />
                         </FormControl>
                         <br/><br/>
-                        <Typography variant='body1' sx={{textAlign:'left',fontSize:'20px'}}>Let us know more of you...</Typography>
+                        <Typography variant='body1' sx={{fontSize:'20px'}}>Let us know more of you...</Typography>
                         <br/>
                         <Grid container>
                             <Grid item xs={6} md={6} sx={info1Style}>
                             <TextField
+                                size='small'
                                 fullWidth
                                 label="First Name"
                                 id="outlined-start-adornment"
                                 sx={{mb:'3vh'}}
+                                InputLabelProps={{style: {fontSize: '14px',marginTop:'4px'}}}
+                                inputProps={{style: {fontSize: '14px',margin:'5px'}}}
                                 /><br/>
                             <TextField
+                                size='small'
                                 fullWidth
                                 label="Last Name"
                                 id="outlined-start-adornment"
+                                InputLabelProps={{style: {fontSize: '14px',marginTop:'4px'}}}
+                                inputProps={{style: {fontSize: '14px',margin:'5px'}}}
                                 />
                             </Grid>
-                            <Grid item xs={6} md={6} sx={{...info1Style, pl:'20%',pt:'1%'}} >
+                            <Grid item xs={6} md={6} sx={{...info1Style, pl:'15%',pt:'1%'}} >
                             <Badge
                                 overlap="circular"
                                 anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
                                 badgeContent={
-                                    <Button type='file'>
-                                        <CameraAltRounded fontSize='large' sx={{cursor:'pointer'}}/>
+                                    <Button>
+                                        <CameraAltRounded color='success' fontSize='medium'/>
                                     </Button>
                                 }
+                                onClick={() => refInputTypeFile.current.click()}
+                                sx={{cursor:'pointer'}}
                                 >
-                                    <Avatar alt='User Profile' src='' sx={{width:'110px', height:'110px'}}></Avatar>
-                                </Badge>
-                                
-
+                                <Avatar alt='User Profile' src='' sx={{width:'6rem', height:'6rem'}}></Avatar>
+                            </Badge>
+                            <input type='file'
+                                name='imgSource'
+                                onchange={handleImgChange}
+                                ref={refInputTypeFile} hidden></input>
                             </Grid>
                         </Grid>
-                </Box>
+                        
+                        <br/><br/>
+                            <Typography 
+                                sx={{
+                                    textAlign:'left',
+                                    width:'80%',
+                                    fontSize:'0.6rem',
+                                    color:'grey'
+                                }}>
+                                You can use a username so that other 
+                                people will be able to search you publicly
+                                without adding your email address.<br/><br/>
+                                letters and numbers only. 
+                                5-30 characters only. 
+                            </Typography>
+                            <TextField
+                                label="Username"
+                                id="outlined-start-adornment"
+                                value={countUChar}
+                                error={errorStateU}
+                                helperText={usernameTxt}
+                                InputProps={{
+                                    startAdornment: <InputAdornment position="start">@</InputAdornment>,
+                                    endAdornment: <InputAdornment position='end'>{countUChar.length}</InputAdornment>,
+                                    style:{
+                                        fontSize:14, 
+                                        fontWeight:600,
+                                        color:'#154017'
+                                    }
+                                }}
+                                sx={{
+                                    mt:1, width:'70%',
+                                    fontSize:'14px',
+                                }}
+                                onChange={handleUsername}/>
+                            <br/><br/>
+                            <Divider/>
+                            <br/>
+                            <Button
+                                variant='contained'
+                                color='success'
+                                startIcon={<HowToRegOutlined/>}
+                                size='large'
+                                sx={{px:5}}
+                                >
+                                Join Us
+                            </Button>
+                            <br/><br/><br/>
+                            <Typography
+                                sx={{
+                                    textAlign:'center',
+                                    fontSize:'12px',
+                                    mb:3
+                                }}>
+                                Already Joined? You can <Link to='/login' style={{fontWeight:700, color:'#63b356'}}>Login here</Link> instead
+                            </Typography>
+                </Box>  
             </Container>
         </>
     )
