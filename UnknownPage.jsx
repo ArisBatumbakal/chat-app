af3bafddc4f98c297d20ee45b277222e70a319f3
ArisBatumbakal <121299450+ArisBatumbakal@users.noreply.github.com>
2024-04-13 18:09:53 +0200

    Update UnknownPage.jsx 13/4

--- UnknownPage.jsx
+++ UnknownPage.jsx
@@ -1,7 +1,56 @@
+import * as React from 'react';
+import {Link} from 'react-router-dom';
+import {Container, Box, Divider, Typography, Button} from '@mui/material';
+import {Dangerous } from '@mui/icons-material';
+
+
 function UnknownPage(){
+    const urltxt = window.location.search;
+    const urlGoBack = new URLSearchParams(urltxt).get('from');
+
     return (
         <>
-         UnknownPage
+            <Container
+                className='inconsolata-font'
+                sx={(theme) => ({
+                    margin:'auto',
+                    width: '98vW',
+                    height: '100vH',
+                    backgroundColor: '#BDCDBB',
+                    p:'2vH', pt:'10vh',
+                    color:'#154017',
+                    textAlign:'center'
+                })}>
+                <Dangerous color='error' sx={{fontSize:'100px'}}></Dangerous>
+                <br/><br/><br/>
+                <Typography variant='h5'>You don't have the authorization to enter this page or the Link is broken</Typography>
+                <br/><br/>
+                <Divider></Divider>
+                <br/><br/>
+                <Box sx={{
+                    textAlign:'center'
+                }}>
+                    <Button
+                        variant='outlined'
+                        color='success'
+                        sx={{px:5}}>
+                        <Link to={`/${urlGoBack.toString()}`}
+                            style={{textDecoration:'none', color:'black'}}>
+                            Go Back
+                        </Link>
+                    </Button>
+                    <Button
+                        variant='outlined'
+                        color='success'
+                        sx={{px:5, ml:2}}>
+                        <Link to='/'
+                            style={{textDecoration:'none', color:'black'}}>
+                            Go to HomePage
+                        </Link>
+                    </Button>
+                </Box>
+
+            </Container>
         </>
     )
 }
